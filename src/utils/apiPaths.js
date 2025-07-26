9e241fb470b27ef6186b697e1d2fa4ea7598444f
Amir Hatami <hatamiamir055@email.com>
2025-07-26 14:32:50 +0330

    add backend url

--- src/utils/apiPaths.js
+++ src/utils/apiPaths.js
@@ -1,5 +1,4 @@
-// export const BASE_URL = "https://sabzlearn-yrz3.onrender.com";
-export const BASE_URL = "http://localhost:8000";
+export const BASE_URL = "https://sabzlearn-fake.liara.run";
 
 // utils/apiPaths.js
 export const API_PATHS = {
